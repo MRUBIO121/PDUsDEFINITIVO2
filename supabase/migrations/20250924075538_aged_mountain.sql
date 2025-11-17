c27769fe692bd6115351bc65a21062592a1b380d
Miguel Rubio <104618155+MRUBIO121@users.noreply.github.com>
2025-11-17 08:48:04 +0100

    Deleted 20250924075538_aged_mountain.sql

--- supabase/migrations/20250924075538_aged_mountain.sql
+++
@@ -1,7 +0,0 @@
-@@ .. @@
-         chain NVARCHAR(255),
-         node NVARCHAR(255),
-         serial NVARCHAR(255),
--        current DECIMAL(18, 4), -- Only electrical metric we store
-+        [current] DECIMAL(18, 4), -- Only electrical metric we store
-         temperature DECIMAL(18, 4),
